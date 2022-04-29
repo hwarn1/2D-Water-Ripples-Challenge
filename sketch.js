a9ab6f7a4b2a0b91b2937679aeede94fe00c54ea
hwarn1 <79218512+hwarn1@users.noreply.github.com>
2022-04-29 11:37:30 -0400

    Create sketch.js
    first attempt at recreating code

---
+++ sketch.js
@@ -0,0 +1,51 @@
+let cols = 200;
+let rows = 200;
+let current; // = new float[cols][rows];
+let previous; // = new float[cols][rows];
+
+let dampening = 0.99;
+
+function setup() {
+  pixelDensity(1);
+  createCanvas(600,400);
+  cols = width;
+  rows = height;
+ current = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
+ previous = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
+
+}
+
+function mouseClicked() {
+  previous[mouseX][mouseY] = 500;
+} 
+
+function draw() {
+  background(0);
+  
+  loadPixels();
+  for (let x = 1; x < cols-1; x++) {
+    for (let y = 1; y < rows-1; y++) {
+      current[x][y] = (
+        previous[x-1][y] + 
+        previous [x+1][y] +
+        previous [x][y-1] +
+        previous [x][y+1]) /2 - 
+        current[x][y];
+      current[x][y] = current[x][y] * dampening;
+      
+      let index = (x + y * cols) * 4;
+      pixels[index] = color(current[x][y]);
+      pixels[index + 0] = current[x][y];
+      pixels[index + 1] = current[x][y];
+      pixels[index + 2] = current[x][y];
+    }
+  }
+  
+  updatePixels();
+  
+  let temp = previous;
+  previous = current;
+  current = temp;
+  
+  
+}
