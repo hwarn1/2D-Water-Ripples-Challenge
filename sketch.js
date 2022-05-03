dd3589459e6d3b848aeb4d8f42c7df308be6fa75
hwarn1 <79218512+hwarn1@users.noreply.github.com>
2022-05-03 10:57:15 -0400

    Update sketch.js
    Second time working through it -- Added some comments this time to help me understand

--- sketch.js
+++ sketch.js
@@ -1,34 +1,47 @@
+//https://www.youtube.com/watch?v=BZUdGqeOD0w&list=PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH&index=152
+
+
+let cols;
+let rows;
+//two arrays of words (integers)
+//one holds the current state of water
+let current;
+//one hold the previous state of water
+let previous;
+
+//some non-integer between 0 and 1
 let dampening = 0.99;
 
 function setup() {
   pixelDensity(1);
   createCanvas(600,400);
+  //create background with columns and rows
   cols = width;
   rows = height;
- current = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
- previous = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
-
+  current = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
+  previous = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
+  
 }
 
 function mouseDragged() {
   previous[mouseX][mouseY] = 500;
-} 
+}
 
 function draw() {
   background(0);
   
   loadPixels();
-  for (let x = 1; x < cols-1; x++) {
-    for (let y = 1; y < rows-1; y++) {
+  //for every non-edge element, loop through:
+  for (let x = 1; x < cols - 1; x++) {
+    for (let y = 1; y < rows - 1; y++) {
       current[x][y] = (
         previous[x-1][y] + 
-        previous [x+1][y] +
-        previous [x][y-1] +
-        previous [x][y+1]) /2 - 
+        previous[x+1][y] +
+        previous[x][y-1] + 
+        previous[x][y+1]) / 2 - 
         current[x][y];
       current[x][y] = current[x][y] * dampening;
-      
-      let index = (x + y * cols) * 4;
+      let index = (x + y * cols) * 4
       pixels[index + 0] = current[x][y];
       pixels[index + 1] = current[x][y];
       pixels[index + 2] = current[x][y];
@@ -40,6 +53,5 @@ function draw() {
   let temp = previous;
   previous = current;
   current = temp;
-  
   
 }
