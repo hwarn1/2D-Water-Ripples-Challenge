3cbd9666df3dd254cac4320b53dc8eff38eaea54
hwarn1 <79218512+hwarn1@users.noreply.github.com>
2022-05-04 10:21:07 -0400

    Update sketch.js
    Different colours attempt

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 //https://www.youtube.com/watch?v=BZUdGqeOD0w&list=PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH&index=152
+//https://github.com/CodingTrain/website/blob/main/CodingChallenges/CC_102_WaterRipples/P5/sketch.js
 
 
 let cols;
@@ -10,7 +11,7 @@ let current;
 let previous;
 
 //some non-integer between 0 and 1
-let dampening = 0.99;
+let dampening = 0.995;
 
 function setup() {
   pixelDensity(1);
@@ -20,16 +21,23 @@ function setup() {
   rows = height;
   current = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
   previous = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
+  var scene;
+}
+
+function scene0() {
+  
+  scene = 0;
+  background(200);
+  text("2D Water Ripples", 50, 100);
+  text("Press the space bar to view the simulation", 50, 150);
+  text("Press 'p' to change ripples colur to pink", 50, 180);
   
 }
 
-function mouseDragged() {
-  previous[mouseX][mouseY] = 500;
-}
 
-function draw() {
-  background(0);
-  
+function scene1() {
+  scene = 1
+  //square pixels arranged in columns and rows
   loadPixels();
   //for every non-edge element, loop through:
   for (let x = 1; x < cols - 1; x++) {
@@ -38,20 +46,35 @@ function draw() {
         previous[x-1][y] + 
         previous[x+1][y] +
         previous[x][y-1] + 
-        previous[x][y+1]) / 2 - 
+        previous[x][y+1]) / 1.999 - 
         current[x][y];
       current[x][y] = current[x][y] * dampening;
       let index = (x + y * cols) * 4
-      pixels[index + 0] = current[x][y];
-      pixels[index + 1] = current[x][y];
-      pixels[index + 2] = current[x][y];
+        if (keyIsPressed && keyCode === 80) {
+        pixels[index + 0] = current[x][y];
+        pixels[index + 1] = current[x][y+1];
+        pixels[index + 2] = current[x][y-1];
+        } else {
+          pixels[index + 0] = current[x][y];
+          pixels[index + 1] = current[x][y];
+          pixels[index + 2] = current[x][y];
+          }
     }
   }
-  
   updatePixels();
   
   let temp = previous;
   previous = current;
   current = temp;
+  }
+
+
+function mouseClicked() {
+  previous[mouseX][mouseY] = 600;
+}
+
+function draw() {
+
+  scene1();
   
 }
